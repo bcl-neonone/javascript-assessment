9b2b37a887a7efa4adfaf05a94bba5b1f702146c
Barent Langwell <blangwell@neonone.com>
2025-10-24 09:58:52 -0700

    First commit

---
+++ script.js
@@ -0,0 +1,35 @@
+
+/* 
+    What do they need to know? 
+    let, const, var
+    conditional logic - if else, ternary
+    iteration
+    functions - declaration, expression, arrow expression syntax
+    objects - accessing properties with dot vs. bracket notation
+
+    this should be proctored with no AI to assess true competency 
+*/
+
+
+// The following code produces an error. 
+// Clear the error and ensure the correct amount is logged to the console.
+const registrantCount = 25;
+registrantCount = registrantCount + 10;
+console.log(registrantCount);
+
+
+// What is the value of profitableEvent in the following snippet? 
+// Uncomment the console log to check your logic! 
+const ticketsSold = 50;
+const profitableEvent = ticketsSold > 30 ? true : false;
+// console.log(profitableEvent);
+
+
+// If the event above was profitable, log "Congratulations!" to the console.
+// If the event was not profitable, log "What a shame..." to the console.
+
+
+
+// Log each item to the following array to the developer console one at a time 
+const seatTypes = ['standard', 'wheelchair', 'companion', 'transfer', 'special'];
+
